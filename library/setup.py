638b8d41fc33688b27a366b031efd367891f921d
KASIREDDY009 <kasireddy9424@gmail.com>
2026-04-07 14:57:07 +0530

    Remove broken publish-library job; library already on PyPI as recipe-manager-nci v1.0.0

--- library/setup.py
+++ library/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="recipe-manager-nci",
-    version="2.0.0",
+    version="1.0.0",
     description="SmartPantry - Kitchen inventory management utilities for tracking food items, expiry dates, and categories",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
